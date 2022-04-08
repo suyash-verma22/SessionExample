3068e8e40b69c2832ca5d5fec22ae767ba3742c0
suyash-verma22 <verma.suyash22@gmail.com>
2022-04-08 10:54:39 +0530

    first commit

---
+++ src/main/java/com/session_management/LogoutServlet.java
@@ -0,0 +1,38 @@
+package com.session_management;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LogoutServlet() {
+        // TODO Auto-generated constructor stub
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
+        response.setContentType("text/html");  
+        PrintWriter out=response.getWriter();  
+        request.getRequestDispatcher("link.html").include(request, response);  
+        HttpSession session=request.getSession();  
+        session.invalidate();  
+        out.print("You are successfully logged out!");  
+        out.close();  
+     }  
+}
